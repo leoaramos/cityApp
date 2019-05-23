c47252347c69640a2c328d21d33443cc344efcc3
bonita <bonita@bonita-VirtualBox>
2019-05-23 20:50:24 -0300

    Correção de Responses das Tarefas 3 e 4

--- cityApp/src/main/java/com/leonardoramos/cityapp/controller/CityController.java
+++ cityApp/src/main/java/com/leonardoramos/cityapp/controller/CityController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,17 +49,34 @@ public class CityController {
 			return ResponseEntity.badRequest().build();
 		}
 	}
-	
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<City> update(@PathVariable("id") long id, @RequestBody City city) {
+		return repository.findById(id).map(record -> {
+			record.setName(city.getName());
+			record.setPopulation(city.getPopulation());
+			record.setFoundationDate(city.getFoundationDate());
+			record.setCoordinate(city.getCoordinate());
+			record.setNeighboors(city.getNeighboors());
+			City updated = repository.save(record);
+			return ResponseEntity.ok().body(updated);
+		}).orElse(ResponseEntity.badRequest().build());
+	}
+
 	@DeleteMapping(path = { "/{id}" })
 	public ResponseEntity<?> delete(@PathVariable("id") long id) {
-		return repository.findById(id).map(record -> {
-			if (record.getNeighboors().size() <= 1) {
-				repository.deleteById(id);
-				return ResponseEntity.ok().build();
-			} else {
-				return ResponseEntity.badRequest().build();
-			}
-		}).get();
+		try {
+			return repository.findById(id).map(record -> {
+				if (record.getNeighboors().size() <= 1) {
+					repository.deleteById(id);
+					return ResponseEntity.ok().build();
+				} else {
+					return ResponseEntity.badRequest().build();
+				}
+			}).get();
+		} catch (Exception e) {
+			return ResponseEntity.badRequest().build();
+		}
 	}
 
 }
